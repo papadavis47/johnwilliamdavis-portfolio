9b3b7939a4089abae9b9d0d4edde1a1cba87b5aa
John William Davis <jwdavisdeveloper@gmail.com>
2022-12-02 16:07:04 -0800

    All in on TypeScript - change my title on landing page

--- components/LandingPageTitle/LandingPageTitle.tsx
+++ components/LandingPageTitle/LandingPageTitle.tsx
@@ -1,10 +1,10 @@
-import TitleWrapper from "./TitleWrapper";
+import TitleWrapper from './TitleWrapper';
 
 const LandingPageTitle = () => {
   return (
     <TitleWrapper>
       <h1 className='title'>John William Davis</h1>
-      <h2 className='subtitle'>Front End Developer</h2>
+      <h2 className='subtitle'>TypeScript Developer</h2>
       <p className='paragraph'>Portfolio</p>
     </TitleWrapper>
   );
