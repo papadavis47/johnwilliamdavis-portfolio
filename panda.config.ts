5b9d6ec87f321dc30f76dcb0601090efe10fed47
John William Davis <jwdavisdeveloper@gmail.com>
2023-08-29 14:40:31 -0700

    create globalStyling file and globals folder in src, move favicon to root of app

--- panda.config.ts
+++ panda.config.ts
@@ -1,42 +1,5 @@
-import { defineConfig, defineGlobalStyles } from '@pandacss/dev';
-
-const globalCss = defineGlobalStyles({
-  // Global styles from Josh Comeau
-  '*': {
-    margin: 0,
-    boxSizing: 'border-box',
-  },
-  '*::before': {
-    boxSizing: 'border-box',
-  },
-  '*::after': {
-    boxSizing: 'border-box',
-  },
-  'a': {
-    textDecoration: 'none',
-  },
-  'html, body': {
-    height: '100%',
-  },
-  'body': {
-    minHeight: '100%',
-    lineHeight: 1.5,
-    WebkitFontSmoothing: 'antialiased',
-  },
-  'img, picture, video, canvas, svg': {
-    display: 'block',
-    maxWidth: '100%',
-  },
-  'input, button, textarea, select': {
-    font: 'inherit',
-  },
-  'p, h1, h2, h3, h4, h5, h6': {
-    overflowWrap: 'break-word',
-  },
-  '#root, #__next': {
-    isolation: 'isolate',
-  },
-});
+import { defineConfig } from '@pandacss/dev';
+import { globalCss } from '@/globals/globalStyling';
 
 export default defineConfig({
   // Whether to use css reset
