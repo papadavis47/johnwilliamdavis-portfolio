6ab4dfdc5ce0ece380cb0e512acc2a3bb39d7582
John William Davis <jwdavisdeveloper@gmail.com>
2023-08-29 22:16:50 -0700

    switch media queries for LandingTitle component to use min-width, increase mobile font size

--- src/components/LandingTitle.tsx
+++ src/components/LandingTitle.tsx
@@ -10,44 +10,44 @@ const TitleWrapper = styled.div`
   font-size: 24px;
   font-family: merriweather;
   color: hsl(0, 0%, 20%);
-  font-weight: 300;
+  font-weight: 400;
 `;
 
 const TitleMain = styled.h1`
-  font-size: 3rem;
+  font-size: 2rem;
   &:hover {
     color: hsl(209.6, 100%, 55.9%);
   }
-  @media screen and (max-width: 500px) {
-    font-size: 2rem;
+  @media screen and (min-width: 640px) {
+    font-size: 3rem;
   }
   transition: color 900ms;
 `;
 
 const SubtitleMain = styled.h2`
-  font-size: 2rem;
+  font-size: 1.25rem;
   &:hover {
     color: hsl(142.2, 100%, 59.8%);
   }
-  @media screen and (max-width: 500px) {
-    font-size: 1rem;
+  @media screen and (min-width: 640px) {
+    font-size: 2rem;
   }
   transition: color 500ms;
 `;
 
 const SubtitleSecondary = styled.h3`
-  font-size: 2rem;
+  font-size: 1.25rem;
   &:hover {
     color: hsl(5.6, 78.1%, 57.1%);
   }
-  @media screen and (max-width: 500px) {
-    font-size: 1rem;
+  @media screen and (min-width: 640px) {
+    font-size: 2rem;
   }
   transition: color 700ms;
 `;
 
 const StyledSpan = styled.span`
-  font-size: 1rem;
+  font-size: 1.25rem;
   text-transform: capitalize;
   &:hover {
     background-color: hsl(58.1, 100%, 69.8%);
